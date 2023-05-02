d103be62af910cf1cfecf435cb722ba71b9492bb
KhacDuy <duypk@duy-mbp-14.local>
2023-05-02 20:36:03 +0700

    test

--- frontend/src/app/containers/Employee/components/Employees/index.tsx
+++ frontend/src/app/containers/Employee/components/Employees/index.tsx
@@ -205,7 +205,7 @@ export class Employees extends React.Component<
         <SearchBar searchInput={generalSearch} />
         <div className={`${style['g-sectionbar']} ${style['grid-x']}`}>
           <Breadcrumb
-            rootPathName='View & Manage Employees v1'
+            rootPathName='View & Manage Employees'
             isSecondaryPage={false}
           >
             <li>
